b1296b07064a2323a49cd07fc6f406e94e72f054
Mohemmad Imtiyaz <imtiyajkhan789@gmail.com>
2021-09-22 10:52:49 +0530

    added reducers and action

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import MovieCard from "./MovieCard";
+import Navbar from "./Navbar";
+import {data} from '../data';
 
 function App() {
   return (
     <div className="App">
-      <h1>this is movie app</h1>
+      <Navbar/>
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+        <div className="list">
+          {data.map(movie => (
+            <MovieCard movie={movie}/>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
